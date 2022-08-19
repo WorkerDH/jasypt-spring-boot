943973b400075fe5bf7ab71bd7887c467574dccf
denghong <743855298@qq.com>
2022-08-19 13:22:57 +0800

    配置文件密码加密的所需配置及部分注释

---
+++ src/main/java/com/example/jasyptspringboot/jasyptspringboot/JasyptspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.jasyptspringboot.jasyptspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JasyptspringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JasyptspringbootApplication.class, args);
+    }
+
+}
